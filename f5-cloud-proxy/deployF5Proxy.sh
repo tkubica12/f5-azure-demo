e0e9ca66f99561555a042d8fd1db312e3fe043ec
Tomas Kubica <tkubica12@gmail.com>
2017-12-04 20:11:57 +0100

    automating Azure credential setup for f5 app connector

--- f5-cloud-proxy/deployF5Proxy.sh
+++ f5-cloud-proxy/deployF5Proxy.sh
@@ -4,4 +4,6 @@ add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(
 apt-get update
 sudo apt-get install -y docker-ce
 docker load -i f5-acproxy-1.1.0-build.70.tgz
-docker run -d --restart=always --net=host -e retry=true -e publish=true -e proxyName=f5-cloud-proxy -e user=tomas -e passwd=Azure12345678 -it f5/acproxy:1.1.0-build.70
+mkdir /opt/f5azure
+sed 's/1/'$1'/g;s/2/'$2'/g;s/3/'$3'/g;s/4/'$4'/g;' temp > /opt/f5azure/azure_config.json
+docker run -d --restart=always --net=host -e retry=true -e publish=true -e proxyName=f5-cloud-proxy -e user=tomas -e passwd=Azure12345678 -it -v /opt/f5azure:/app/proxy/vendors/azure f5/acproxy:1.1.0-build.70
