da53ef37dcd55204918a81a2981915cb1f04495e
Tomas Kubica <tkubica12@gmail.com>
2017-12-04 21:01:09 +0100

    docker app connector debug

--- f5-cloud-proxy/deployF5Proxy.sh
+++ f5-cloud-proxy/deployF5Proxy.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+echo $1 $2 $3 $4 > log
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 apt-get update
