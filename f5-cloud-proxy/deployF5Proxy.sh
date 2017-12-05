24a9803a566259ba67b6dfd2031babd835db1f44
Tomas Kubica <tkubica12@gmail.com>
2017-12-05 05:54:56 +0100

    temp and script changes

--- f5-cloud-proxy/deployF5Proxy.sh
+++ f5-cloud-proxy/deployF5Proxy.sh
@@ -5,6 +5,9 @@ apt-get update
 sudo apt-get install -y docker-ce
 docker load -i f5-acproxy-1.1.0-build.70.tgz
 mkdir /opt/f5azure
-sed -i 's/1/'$1'/g;s/2/'$2'/g;s/3/'$3'/g;s/4/'$4'/g;' temp
-cp temp /opt/f5azure/azure_config.json
+sed -i 's/value1/'$1'/g' ./temp
+sed -i 's/value2/'$2'/g' ./temp
+sed -i 's/value3/'$3'/g' ./temp
+sed -i 's/value4/'$4'/g' ./temp
+cp ./temp /opt/f5azure/azure_config.json
 docker run -d --restart=always --net=host -e retry=true -e publish=true -e proxyName=f5-cloud-proxy -e user=tomas -e passwd=Azure12345678 -it -v /opt/f5azure:/app/proxy/vendors/azure f5/acproxy:1.1.0-build.70
