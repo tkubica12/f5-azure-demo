44162d1fd52072725e4329897706a0fb9c926933
Tomas Kubica <tkubica12@gmail.com>
2017-12-05 06:05:34 +0100

    refactor script

--- f5-cloud-proxy/deployF5Proxy.sh
+++ f5-cloud-proxy/deployF5Proxy.sh
@@ -4,10 +4,6 @@ add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(
 apt-get update
 sudo apt-get install -y docker-ce
 docker load -i f5-acproxy-1.1.0-build.70.tgz
-mkdir /home/tomas/f5azure
-sed -i 's/value1/'$1'/g' ./temp
-sed -i 's/value2/'$2'/g' ./temp
-sed -i 's/value3/'$3'/g' ./temp
-sed -i 's/value4/'$4'/g' ./temp
-cp ./temp /home/tomas/f5azure/azure_config.json
+mkdir /opt/f5azure
+echo '{"TENANT":"'$1'","AZURE_SUBSCRIPTION_ID":"'$2'","CLIENT_ID":"'$3'","APPLICATION_SECRET":"'$4'"}' > /opt/f5azure/azure_config.json
 docker run -d --restart=always --net=host -e retry=true -e publish=true -e proxyName=f5-cloud-proxy -e user=tomas -e passwd=Azure12345678 -it -v /opt/f5azure:/app/proxy/vendors/azure f5/acproxy:1.1.0-build.70
